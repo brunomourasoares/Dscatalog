e3e0329895dc3ec605664f8b44ca5e9301b54c86
Bruno Moura Soares <brunomourasoares@gmail.com>
2023-05-28 14:06:28 -0300

    Post Category

--- src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -30,6 +31,14 @@ public class CategoryService {
     public CategoryDTO findById(Long id) {
         Optional<Category> obj = categoryRepository.findById(id);
         Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        return new CategoryDTO(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO insert(CategoryDTO dto) {
+        Category entity = new Category();
+        BeanUtils.copyProperties(dto, entity);
+        categoryRepository.save(entity);
         return new CategoryDTO(entity);
     }
 }
